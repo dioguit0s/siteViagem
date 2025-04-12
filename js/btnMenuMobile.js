60d288d8f26ba6e2bc81571c48d11bd61605d8f6
dioguit0s <diogosantos152005@gmail.com>
2025-04-12 19:25:03 -0300

    finalizado o menu mobile

--- js/btnMenuMobile.js
+++ js/btnMenuMobile.js
@@ -2,11 +2,12 @@
 let btnMenuMobile = document.querySelector('#btnMenuMobile')
 let linha1 = document.querySelector('.lineMenuMob1') 
 let linha2 = document.querySelector('.lineMenuMob2')
+let menuMobile = document.querySelector('#menuMobile')
+let body = document.querySelector('body')
 
 btnMenuMobile.addEventListener("click", ()=>{
     linha1.classList.toggle('ativo1')
-})
-
-btnMenuMobile.addEventListener("click", ()=>{
     linha2.classList.toggle('ativo2')
+    menuMobile.classList.toggle('abrir')
+    body.classList.toggle('trava')
 })
